240467761bb0646edda71b3126bc4ac26e5573e7
oops-shlok <83498152+oops-shlok@users.noreply.github.com>
2021-06-06 06:13:26 +0530

    Update main_1.js

--- main_1.js
+++ main_1.js
@@ -28,7 +28,7 @@ function answer(){
 
   if(question && answer && answer[6].style.backgroundColor===question[0].style.backgroundColor && answer[7].style.backgroundColor===question[1].style.backgroundColor && answer[8].style.backgroundColor===question[2].style.backgroundColor && answer[11].style.backgroundColor===question[3].style.backgroundColor &&answer[12].style.backgroundColor===question[4].style.backgroundColor &&answer[13].style.backgroundColor===question[5].style.backgroundColor && answer[16].style.backgroundColor===question[6].style.backgroundColor && answer[17].style.backgroundColor===question[7].style.backgroundColor && answer[18].style.backgroundColor===question[8].style.backgroundColor){
     document.getElementById('win').play();
-    swal(" Congratulations!!🥳 | | Your points are:  "+ x +" | | "+ " You time is :  "+ hours + ":" + minutes+":" + seconds);  
+    swal(" Congratulations!!🥳 | | Your points are:  "+ x +" | | "+ " Your time is :  "+ hours + ":" + minutes+":" + seconds);  
     }
 }
 
