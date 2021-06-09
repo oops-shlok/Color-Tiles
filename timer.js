f091922b1423b4e9e9ce32c94ee7cbbcdc038bcc
oops-shlok <83498152+oops-shlok@users.noreply.github.com>
2021-06-09 16:08:21 +0530

    Update timer.js

--- timer.js
+++ timer.js
@@ -22,18 +22,6 @@ function startTimer(){
 let timer=window.setInterval(startTimer,1000);
 
 
-
-function answer(){
-  const question = document.querySelectorAll('.item');
-  const answer = document.querySelectorAll('.box');
-
-  if(question && answer && answer[6].style.backgroundColor===question[0].style.backgroundColor && answer[7].style.backgroundColor===question[1].style.backgroundColor && answer[8].style.backgroundColor===question[2].style.backgroundColor && answer[11].style.backgroundColor===question[3].style.backgroundColor &&answer[12].style.backgroundColor===question[4].style.backgroundColor &&answer[13].style.backgroundColor===question[5].style.backgroundColor && answer[16].style.backgroundColor===question[6].style.backgroundColor && answer[17].style.backgroundColor===question[7].style.backgroundColor && answer[18].style.backgroundColor===question[8].style.backgroundColor){
-    document.getElementById('win').play();
-    swal(" Congratulations!!🥳 | | Clicks You Used:  "+ x +" | | "+ " You time is :  "+ hours + ":" + minutes+":" + seconds);
-    window.clearInterval(timer);  
-    }
-}
-
 let x=0
 function points(){
   x=x+1;
